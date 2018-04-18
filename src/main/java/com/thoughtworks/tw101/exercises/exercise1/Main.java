7a8e3bbba15ff799eced08249bdc1f5809d1fc4d
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-18 10:15:40 -0400

    Exercise one first commit. Added two methods that print all the odd numbers between 1 and 100 and print the sum of the odds 1-100.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,31 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        addOddNumbers();
+        addOddNumbersWithRecursion(1, 100, 0);
+    }
 
+
+    public static void addOddNumbers() {
+        int total = 0;
+        int n = 1;
+        while(n <= 100) {
+            total += n;
+            System.out.print(n + ", ");
+            n += 2;
+        }
+        System.out.println("TOTAL: " + total);
+    }
+
+    public static void addOddNumbersWithRecursion(int start, int end, int total) {
+        if(start < end) {
+            System.out.print(start + ", ");
+
+            total += start;
+            start += 2;
+            addOddNumbersWithRecursion(start, end, total);
+        } else {
+            System.out.println("TOTAL: " + total);
+        }
     }
 }
