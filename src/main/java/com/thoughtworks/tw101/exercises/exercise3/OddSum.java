e13e1d7aba51a81528b9ef04b62222ae503d8d23
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-18 15:43:44 -0400

    finished exercise 3. Chose while loop instead of recursion.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,17 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int total = 0;
+        while(start <= end) {
+            total += start;
+            start += 2;
+        }
+        return total;
     }
 }
+
+/*
+    // could use this math instead:
+    // n/2  * (firstTerm + LastTerm)
+    // 50/2 * (   1      +    99   )
+*/
