92ac6180459ac191020c21ab368654035d4df01b
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-19 18:20:38 -0400

    just added some simple code to meet requirements before I do the reading. It helps me having some code to think about when I read. Still need to add classes and better OOP.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise6/Monster.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise6/Monster.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.tw101.exercises.exercise6;
 
 public interface Monster {
+    // should I be putting the variables of health and hp in the interface???
+
     public void takeDamage(int amount);
     public void reportStatus();
 
--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -4,9 +4,38 @@ package com.thoughtworks.tw101.exercises.exercise7;
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        boolean isRunning = true;
+        int randomNumber;
+        int userGuess;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 0 to exit");
+
+        while (isRunning) {
+                randomNumber =  (int) (Math.random() * 100);
+                System.out.println("Please guess a number 1-100");
+
+                userGuess = scanner.nextInt();
+
+                if(userGuess > randomNumber)
+                    System.out.println("You Guessed too high");
+                else if(userGuess < randomNumber)
+                    System.out.println("You Guessed too low");
+                else {
+                    isRunning = false;
+                    System.out.println("you win!");
+                }
+
+
+                if(userGuess == 0) // just an easy exit strategy.
+                    isRunning = false;
+
+        }
 
     }
 }
