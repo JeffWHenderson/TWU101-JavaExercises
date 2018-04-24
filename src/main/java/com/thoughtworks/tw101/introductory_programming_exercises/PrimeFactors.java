9947196036729ca52b7d74823283c50fd0a9e309
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-24 18:02:48 -0400

    made a super simple version of primes generator that works on small numbers. Will need to make more robust.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,40 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
+    private static List<Integer> primes;
+
     public static void main(String[] args) {
+        generate(1);
         List<Integer> primeFactors = generate(30);
+
+        System.out.print(primes.toString());
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> primeFactors = new ArrayList<Integer>();
+
+        generate(n, primeFactors);
+
+        return primeFactors;
+    }
+
+    private static void generate(int n, List<Integer> primeFactors) {
+        if(n % 2 == 0) {
+            primeFactors.add(2);
+            generate(n / 2, primeFactors);
+        }
+        else if(n % 3 == 0) {
+            primeFactors.add(3);
+            generate(n / 3, primeFactors);
+        }
+        else if(n % 5 == 0) {
+            primeFactors.add(5);
+            generate(n / 5, primeFactors);
+        }
+        primes = primeFactors;
     }
 }
